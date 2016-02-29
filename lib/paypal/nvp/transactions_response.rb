4b99ddf47ea20fbcbad80446a59e16da9970032c
Chan Hong Ming <hongming.chm@gmail.com>
2016-02-29 13:52:59 +0800

    upgrade version

--- lib/paypal/nvp/transactions_response.rb
+++ lib/paypal/nvp/transactions_response.rb
@@ -19,7 +19,6 @@ module Paypal
 
       def initialize(attributes = {})
         attrs = attributes.dup
-        binding.pry
         @@attribute_mapping.each do |key, value|
           self.send "#{value}=", attrs.delete(key)
         end
@@ -32,11 +31,11 @@ module Paypal
             transactions[index.to_i][key.to_sym] = attrs.delete(_attr_)
           end
         end
-
+binding.pry
         @transactions = transactions.collect do |_attrs_|
-          Payment::Response::Transaction.new _attrs_
+          Payment::Response::Transaction.new( _attrs_ ) if _attrs_[:AMT].present?
         end
-
+binding.pry
         # warn ignored attrs
         attrs.each do |key, value|
           Paypal.log "Ignored Parameter (#{self.class}): #{key}=#{value}", :warn
